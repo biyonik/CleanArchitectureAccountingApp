8d8e4ac0c80f4ff5624d1451b9710fa18449024a
ahmet <ahmet.altun60@gmail.com>
2023-01-18 23:48:45 +0300

    Multitenant alt yapısı için connection string ve multi context yapısı kurgulandı

--- src/Core/CleanArchitectureAccountingApp.Domain/AppEntities/Company.cs
+++ src/Core/CleanArchitectureAccountingApp.Domain/AppEntities/Company.cs
@@ -10,4 +10,9 @@ public sealed class Company: Entity
     public string? TaxDepartment { get; set; }
     public string? Phone { get; set; }
     public string? Email { get; set; }
+    public string ServerName { get; set; }
+    public string DatabaseName { get; set; }
+    public string PortNumber { get; set; }
+    public string UserId { get; set; }
+    public string Password { get; set; }
 }
---
+++ src/External/CleanArchitectureAccountingApp.Persistence/Context/CompanyDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchitectureAccountingApp.Domain.AppEntities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitectureAccountingApp.Persistence.Context;
+
+public sealed class CompanyDbContext : DbContext
+{
+    private string ConnectionString = "";
+    private readonly AppDbContext _appDbContext;
+
+    public CompanyDbContext(Guid companyId, AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+        Company company = _appDbContext.Companies.Find(companyId);
+        
+        ConnectionString = string.IsNullOrWhiteSpace(company?.UserId) 
+            ? $@"Server={company.ServerName};Port={company.PortNumber};Database={company.DatabaseName};User Id=postgres;Password=12345;" 
+            : $@"Server={company.ServerName};Port={company.PortNumber};Database={company.DatabaseName};User Id={company.UserId};Password={company.Password};";
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(ConnectionString);
+    }
+}

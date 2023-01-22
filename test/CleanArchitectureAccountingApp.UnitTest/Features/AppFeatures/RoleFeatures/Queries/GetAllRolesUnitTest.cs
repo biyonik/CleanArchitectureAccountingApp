b403e039064fe99661e6a8853b23b487670a8861
ahmet <ahmet.altun60@gmail.com>
2023-01-22 23:52:37 +0300

    GetAllRoles için unittest yazıldı

--- test/CleanArchitectureAccountingApp.UnitTest/Features/AppFeatures/RoleFeatures/Queries/GetAllRolesUnitTest.cs
+++ test/CleanArchitectureAccountingApp.UnitTest/Features/AppFeatures/RoleFeatures/Queries/GetAllRolesUnitTest.cs
@@ -1,6 +1,22 @@
-﻿namespace CleanArchitectureAccountingApp.UnitTest.Features.AppFeatures.RoleFeatures.Queries;
+﻿using CleanArchitectureAccountingApp.Application.Services.AppServices.RoleService;
+using CleanArchitectureAccountingApp.Domain.AppEntities.Identity;
+using Moq;
+
+namespace CleanArchitectureAccountingApp.UnitTest.Features.AppFeatures.RoleFeatures.Queries;
 
 public class GetAllRolesUnitTest
 {
-    
+    private readonly Mock<IRoleService> _roleService;
+
+    public GetAllRolesUnitTest()
+    {
+        _roleService = new();
+    }
+
+    [Fact]
+    public async Task GetAllRolesShouldBeNotNull()
+    {
+        _roleService.Setup(
+            x => x.GetAllAsync()).ReturnsAsync(new List<AppRole>());
+    }
 }
